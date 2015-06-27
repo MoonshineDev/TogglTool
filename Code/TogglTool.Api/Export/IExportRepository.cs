f7519b9ca133c3d04c417d5a8cbba56ffe1a936a
Ted Månsken <ted.wik@gmail.com>
2015-06-27 17:09:05 +0200

    Updated export interface

--- Code/TogglTool.Api/Export/IExportRepository.cs
+++ Code/TogglTool.Api/Export/IExportRepository.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TogglTool.Api.Models;
 
 namespace TogglTool.Api.Export
 {
     public interface IExportRepository
     {
+        void exportWorkspace(Workspace workspace);
     }
 }

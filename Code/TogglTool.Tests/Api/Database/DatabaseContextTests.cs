3569a23343ad67b699dc54542da4857c2c992fe3
Ted Månsken <ted.wik@gmail.com>
2015-07-04 15:05:53 +0200

    Specified UnitTests for DatabaseContext

---
+++ Code/TogglTool.Tests/Api/Database/DatabaseContextTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TogglTool.Tests.Api.Database
+{
+    [TestFixture]
+    public class DatabaseContextTests
+    {
+        [Test]
+        [Ignore("Not yet implemented")]
+        public void ContextOnlyContainsBaseEntity()
+        {
+        }
+
+        [Test]
+        [Ignore("Not yet implemented")]
+        public void ContextContainsAllBaseEntity()
+        {
+        }
+
+        [Test]
+        [Ignore("Not yet implemented")]
+        public void ContextDoesNotContainArbitraryBaseEntity()
+        {
+        }
+    }
+}

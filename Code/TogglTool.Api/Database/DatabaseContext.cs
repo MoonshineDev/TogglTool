724fac5d968a2d33ad47f9171b166f25689afcd2
Ted Månsken <ted.wik@gmail.com>
2015-07-04 14:42:00 +0200

    Added DbContext, ConnectionString, DbSets

---
+++ Code/TogglTool.Api/Database/DatabaseContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TogglTool.Api.Models;
+using TogglTool.Api.Properties;
+
+namespace TogglTool.Api.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<TimeEntry> TimeEntries { get; set; }
+        public DbSet<Workspace> Workspaces { get; set; }
+
+        public DatabaseContext()
+            : this(GetDefaultConnectionString())
+        { }
+
+        public DatabaseContext(string nameOrConnectionString)
+            : base(nameOrConnectionString)
+        { }
+
+        private static string GetDefaultConnectionString()
+        { return Settings.Default.ConnectionString; }
+    }
+}
